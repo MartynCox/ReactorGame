197f7385f960640a0deba85a2c11f0daa47e6319
MartynCox <94151909+MartynCox@users.noreply.github.com>
2023-08-25 12:35:59 +1000

    Simple cursors

---
+++ Assets/Scripts/HoverPointer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverPointer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    // On hover change pointer
+    [SerializeField] private Texture2D _pointerTexture;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(_pointerTexture, new Vector2(14f, 0f), CursorMode.Auto);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}

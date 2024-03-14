bf79fd821d8e15a3cdb349ef187deffb9706f8db
MartynCox <94151909+MartynCox@users.noreply.github.com>
2024-03-14 14:02:53 +1100

    Code displaying

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -1,18 +1,16 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class EndScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Get text component
+        TMP_Text text = GetComponent<TMP_Text>();
+        string code = ScenarioController.Instance.GetCompletionCode();
+        text.text = text.text.Replace("{Code}", code);
     }
 }
